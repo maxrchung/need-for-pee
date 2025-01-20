9b35047ddf7d0dce881bce329a8be43b9bddad67
Julia Ahopelto <me@notjulie.dev>
2025-01-19 20:54:59 -0800

    add maxs thing yeah

--- Need for Pee/Assets/Scripts/Characters/MikuHatsune.cs
+++ Need for Pee/Assets/Scripts/Characters/MikuHatsune.cs
@@ -17,13 +17,13 @@ namespace Characters
             var choice = await Manager.DisplayChoice("hi im hatsune miku", choices.ToArray());
             switch (choice)
             {
-                case 0:
+                case 1:
                     await Manager.DisplayText("wow frick you dude");
                     await Manager.DisplayText("im gonna flip");
                     gameObject.transform.Rotate(0, 180, 0);
                     await Manager.DisplayText("go away");
                     break;
-                case 1:
+                case 0:
                     await Manager.DisplayText("yes im miku hatsune");
                     break;
                 case 2:
