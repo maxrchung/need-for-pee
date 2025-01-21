aee42cf9dbae2ad0a6144a804e315f1ae12d4a8f
WL <alakamande@gmail.com>
2025-01-20 17:43:23 -0800

    gatorade works, game difficulty increased

--- Need for Pee/Assets/Scripts/PissBottleBehavior.cs
+++ Need for Pee/Assets/Scripts/PissBottleBehavior.cs
@@ -1,15 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PissBottleBehavior : MonoBehaviour, IInteractable
+public class PissBottleBehavior : BaseCharacter
 {
     public MeshRenderer EmptyBottleMesh;
     public MeshRenderer FullBottleMesh;
-
+    PissBarScript pissBar;
     private bool _used = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        pissBar = FindAnyObjectByType<PissBarScript>();
     }
 
     // Update is called once per frame
@@ -17,12 +22,30 @@ public class PissBottleBehavior : MonoBehaviour, IInteractable
     {
     }
 
-    public void Interact()
+    protected override async Task DialogTree()
     {
-        _used = true;
-        EmptyBottleMesh.enabled = false;
-        FullBottleMesh.enabled = true;
-    }
+        var choices0 = new List<string>() {"yes...","not yet"};
 
-    public bool CanInteract() => !_used;
+        await Manager.DisplayUnskippableText("an empty gatorade bottle...");
+        var choice0 = await Manager.DisplayChoice("is this what your life has come to?", choices0.ToArray());
+        switch (choice0)
+        {
+            case 0:
+                int pissTime = (int)(pissBar.BeginEjectingPiss() * 0.1f) * 10;
+                string pissString = "";
+                for(int i = 0; i < pissTime; i++)
+                {
+                    pissString += ".";
+                }
+                await Manager.DisplayUnskippableText(pissString);
+                _used = true;
+                EmptyBottleMesh.enabled = false;
+                FullBottleMesh.enabled = true;
+                await Manager.DisplayText("although momentarily, the urge is abated");
+                break;
+            case 1:
+                break;
+        }
+    }
+    protected override bool ShouldBeInteractable() => !_used;
 }
