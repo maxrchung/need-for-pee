4d55e0fc2fc5c95fe9402a08650ee37e48b9b605
Julia Ahopelto <me@notjulie.dev>
2025-01-20 21:03:45 -0800

    you can win now yeah

--- Need for Pee/Assets/Scripts/Characters/VictoryToilet.cs
+++ Need for Pee/Assets/Scripts/Characters/VictoryToilet.cs
@@ -7,41 +7,40 @@ public class VictoryToilet : BaseCharacter
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     protected override async Task DialogTree()
     {
         var choice = -1;
-        if(FlagManager.Check(GameFlag.PeeShy))
+        if (FlagManager.Check(GameFlag.PeeShy))
         {
             await Manager.DisplayText("well ello ello");
-            choice = await Manager.DisplaySlowChoice("back to ave a piss then?","yeah...","mmm......");
-            switch(choice)
+            choice = await Manager.DisplaySlowChoice("back to ave a piss then?", "yeah...", "mmm......");
+            switch (choice)
             {
                 case 0:
                     await Manager.DisplayUnskippableText("well dont hold back then");
-                    GG();
-                    break;
+                    await GG();
+                    return;
                 case 1:
                     await Manager.DisplayText("well ill be ere all night");
                     return;
             }
         }
+
         await Manager.DisplayText("well ello ello, lookin to ave a piss?");
-        choice = await Manager.DisplaySlowChoice("got a nice place for that roight ere","ok sure","erm...");
-        switch(choice)
+        choice = await Manager.DisplaySlowChoice("got a nice place for that roight ere", "ok sure", "erm...");
+        switch (choice)
         {
             case 0:
                 await Manager.DisplayText("well ave at it then");
-                GG();
-                break;
+                await GG();
+                return;
             case 1:
                 await Manager.DisplayText("roight then");
                 await Manager.DisplayText("must not be that urgent i suppose");
@@ -50,7 +49,7 @@ public class VictoryToilet : BaseCharacter
         }
     }
 
-    async void GG()
+    async Task GG()
     {
         Manager.ClearButtons();
         Manager.ClearText();
